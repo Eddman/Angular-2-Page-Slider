6fd745b42894e434d94c29fd280a1b408b0cbc56
Adam Netočný <adam.netocny@deutsche-boerse.com>
2019-11-18 16:04:45 +0100

    Formatting

--- projects/example/src/app/app.component.ts
+++ projects/example/src/app/app.component.ts
@@ -2,10 +2,10 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {of} from 'rxjs';
 
 @Component({
-    selector   : 'example-component',
-    templateUrl: 'app.component.html',
-    styleUrls  : ['app.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector       : 'example-component',
+    templateUrl    : 'app.component.html',
+    styleUrls      : ['app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
     public keysEnabled: boolean = true;
